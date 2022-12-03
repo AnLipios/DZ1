d7044850f9ae1ebea1fdb92a77809ad082a3b94c
Anna <ogonek0918@yandex.ru>
2022-12-03 21:47:49 +0700

    задача2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число a: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if(numberA > numberB)
+{
+    Console.WriteLine("numberA больше!");
+    Console.WriteLine("numberB меньше :(");
+}
+if(numberB > numberA)
+{
+    Console.WriteLine("numberB больше!");
+    Console.WriteLine("numberA меньше");
+}
