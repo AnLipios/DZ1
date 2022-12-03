0a82972af6b7e1f39c884760cacef560c267a98d
Anna <ogonek0918@yandex.ru>
2022-12-03 23:32:47 +0700

    задача8

---
+++ task8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int n=int.Parse(Console.ReadLine());
+int a=1;
+
+while (a<=n)
+{
+    if(a%2==0)
+    {
+        Console.WriteLine($"{a}");
+        
+    }
+    a++;
+}
